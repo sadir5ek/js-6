9f3f8055e577cb8ee074548534c11ea024eaa752
sadir5ek <abdiraimovsadirbek@gmail.com>
2024-11-22 21:08:51 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,44 @@
+//  while, do while, for .
+//  while( ) {
+
+//  }
+
+//  do {
+
+//  }while()
+
+// let num = 50
+// while(num < 0){
+// console.log(num);
+// num--
+
+//  }
+// let num = 0
+// do{
+//     console.log(num);
+//     num++
+// } while (num <=10)
+//  for (let i % 2 === 0 ; i <=  ; i++) {
+//     console.log(i,'1цикл');
+//    for ( let j = 0; j <= 3; j++) {
+//        console.log( j ,'2цикл');
+        
+//     }
+//  }
+// for(let i = 1 ; i <= 50; i++) {
+//     if
+// console.log();
+
+// }
+
+for (let i =1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+     console.log('coca-cola',i);
+    } else if (i % 3 === 0) {
+     console.log('coca',i);
+    } else if (i % 5 === 0) {
+      console.log('cola',i);
+      }  else {
+    console.log(i);
+    }
+}
